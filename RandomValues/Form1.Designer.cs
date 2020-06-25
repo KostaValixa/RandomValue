a860e59d9aa78391bfac9f3a70a8d6145be17b51
KostaValixa <kosta.valixa@mail.ru>
2020-06-25 23:14:29 +0500

    Добавьте файлы проекта.

---
+++ RandomValues/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace RandomValues
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sizeLabel = new System.Windows.Forms.Label();
+            this.sizeBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.difMinMax = new System.Windows.Forms.Label();
+            this.tableView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.tableView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sizeLabel
+            // 
+            this.sizeLabel.AutoSize = true;
+            this.sizeLabel.Location = new System.Drawing.Point(12, 9);
+            this.sizeLabel.Name = "sizeLabel";
+            this.sizeLabel.Size = new System.Drawing.Size(46, 13);
+            this.sizeLabel.TabIndex = 0;
+            this.sizeLabel.Text = "Размер";
+            this.sizeLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // sizeBox
+            // 
+            this.sizeBox.Location = new System.Drawing.Point(64, 9);
+            this.sizeBox.Name = "sizeBox";
+            this.sizeBox.Size = new System.Drawing.Size(100, 20);
+            this.sizeBox.TabIndex = 1;
+            this.sizeBox.TextChanged += new System.EventHandler(this.sizeBox_TextChanged);
+            this.sizeBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.sizeBox_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Разница между мин и макс равна:";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // difMinMax
+            // 
+            this.difMinMax.AutoSize = true;
+            this.difMinMax.Location = new System.Drawing.Point(159, 60);
+            this.difMinMax.Name = "difMinMax";
+            this.difMinMax.Size = new System.Drawing.Size(0, 13);
+            this.difMinMax.TabIndex = 4;
+            this.difMinMax.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tableView
+            // 
+            this.tableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableView.Location = new System.Drawing.Point(12, 97);
+            this.tableView.Name = "tableView";
+            this.tableView.Size = new System.Drawing.Size(306, 211);
+            this.tableView.TabIndex = 5;
+            this.tableView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 320);
+            this.Controls.Add(this.tableView);
+            this.Controls.Add(this.difMinMax);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sizeBox);
+            this.Controls.Add(this.sizeLabel);
+            this.Name = "Form1";
+            this.Text = "Таблица с разницей";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.tableView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label sizeLabel;
+        private System.Windows.Forms.TextBox sizeBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label difMinMax;
+        private System.Windows.Forms.DataGridView tableView;
+    }
+}
+
---
+++ RandomValues/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomValues
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rdn = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            int tableSize = Convert.ToInt32(sizeBox.Text);
+
+            int minValue = tableSize*10;
+            int maxValue = 0;
+
+            tableView.ColumnCount = tableSize;
+            tableView.RowCount = tableSize;
+
+            for (int i=0; i < tableView.Columns.Count; i++)
+            {
+                for (int g=0; g<tableView.Rows.Count; g++)
+                {
+                    int value = rdn.Next(0,tableSize*10);
+                    tableView[i, g].Value = value.ToString();
+                    if (value > maxValue){ maxValue = value; };
+                    if (value < minValue){ minValue = value; };
+                };
+            };
+            difMinMax.Text = Convert.ToString(maxValue - minValue);
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void sizeBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void sizeBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            if (!Char.IsDigit(number) && number != 8) // цифры и клавиша BackSpace
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
